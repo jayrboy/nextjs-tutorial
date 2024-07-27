32c5158db517cde89b0897d1c71f4a4b5a97cfd9
jarboy <j.jayox_@hotmail.com>
2024-07-28 00:37:48 +0700

    post api

--- app/api/users/route.ts
+++ app/api/users/route.ts
@@ -2,7 +2,17 @@
 import { fetchUsers, saveUser } from '@/utils/actions';
 import { NextRequest, NextResponse } from 'next/server';
 
-export const GET = async () => {
+export const GET = async (req: NextRequest) => {
+  console.log(req.url);
+  console.log(req.nextUrl.searchParams.get('id'));
+
   const users = await fetchUsers();
-  return Response.json({ message: 'api' });
+  return Response.json({ users });
+};
+
+export const POST = async (request: Request) => {
+  const user = await request.json();
+  const newUser = { ...user, id: Date.now().toString() };
+  await saveUser(newUser);
+  return Response.json({ msg: 'user created' });
 };
