f78018c9f6ecbac3e5272171cd6cc089ab92ea26
jarboy <j.jayox_@hotmail.com>
2024-07-27 18:23:05 +0700

    add counter and CounterPage.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
 import './globals.css';
-import Navbar from './components/Navbar';
+import Navbar from '../components/Navbar';
 import type { Metadata } from 'next';
 import { Inter, Inconsolata, Roboto } from 'next/font/google';
 
